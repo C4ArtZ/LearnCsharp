e4ed665043affa6f2036f001a1b7d41f85d792f3
Chris <github@c4artz.de>
2021-11-14 16:21:59 +0100

    Hello World - Summary comment

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -4,6 +4,10 @@ namespace HelloWorld
 {
     class Program
     {
+        /// <summary>
+        /// Entry point lol
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
