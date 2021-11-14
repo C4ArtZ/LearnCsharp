9e5abad12556fe1184ad145ebc23d5a70f056bb8
Chris <github@c4artz.de>
2021-11-14 17:50:47 +0100

    Challenge String and its methods 2

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,7 +10,29 @@ namespace HelloWorld
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int index;
+            string text, character, firstName, lastName, fullName;
+            
+            Console.Write("Enter a string here: ");
+            text = Console.ReadLine();
+            
+            Console.Write("Wich character do you want to search for: ");
+            character = Console.ReadLine();
+
+            index = text.IndexOf(character);
+
+            Console.WriteLine($"The character {character} is located at index {index}.");
+            
+            Console.Write("\nFirst name: ");
+            firstName = Console.ReadLine();
+            
+            Console.Write("Last name: ");
+            lastName = Console.ReadLine();
+
+            fullName = firstName + " " + lastName;
+
+            Console.WriteLine($"Your name is {fullName}");
+
         }
     }
 }
